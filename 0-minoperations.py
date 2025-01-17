175f8807aae2994d203fcd3803795c99711bb6e6
desparvis <latifahm501@gmail.com>
2025-01-17 15:33:18 +0200

    minimum operations

---
+++ 0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+first documentaion
+minimum operations to reach character n
+"""
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    i = 2 
+
+    while n > 1:
+        while n % i == 0:
+            operations += i
+            n /= i
+        i += 1
+
+    return operations
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # alu-interview
+My journey on learning how to do minimum on operations on python
