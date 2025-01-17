486436345e3b7d6b192526c29488d597e9ebc16a
desparvis <latifahm501@gmail.com>
2025-01-17 15:40:05 +0200

    pep8 validation update

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
+
 """
-first documentaion
+first documentation
 minimum operations to reach character n
 """
+
 def minOperations(n):
     if n <= 1:
         return 0
 
     operations = 0
-    i = 2 
+    i = 2
 
     while n > 1:
         while n % i == 0:
