3619bbfa33fbdf6c90b9e7fca94b37451b889b0e
desparvis <latifahm501@gmail.com>
2025-02-22 21:49:06 +0200

    pascal triangle

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # alu-interview
-My journey on learning how to do minimum on operations on python
+My journey on learning how to do minimum on operations on python which was done in -minoperations.py
---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    if n <=0:
+        return []
+    pascal_triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(pascal_triangle[i-1][j-1] + pascal_triangle[i-1][j])
+        row.append(1)
+        pascal_triangle.append(row)
+    return pascal_triangle
